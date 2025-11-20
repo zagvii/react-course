d3fae25c3bcb3e5b3b7794c965e5a61c0c473f87
zagvii <mirian.vieira.moreira@gmail.com>
2025-11-20 17:58:07 -0300

    js-refresher-export-import

---
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,13 @@
+// import { apiKey } from "./utils";
+
+// OR 
+
+// import apiKeyName from "./utils.js";
+// for default exports
+
+import * as utils from "./utils.js";
+// import multiple variables
+
+console.log(utils.default);
+console.log(utils.apiKey);
+console.log(utils.number);
---
+++ 00-starting-js-proj/assets/scripts/utils.js
@@ -0,0 +1,12 @@
+// export let apiKey = '123ABC';
+
+// OR
+
+export default '123ABC';
+// Only one default export per file
+
+// OR 
+
+export let apiKey = '123ABC'; 
+export let number = '123456'; 
+// export multiple variables
